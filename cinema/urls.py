6934ff1d977227107afe04f02c699a2f9d7cecf0
Viesich <v.s.vesich@gmail.com>
2024-10-22 11:33:17 +0300

    add routs to all endpoints

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,1 +1,30 @@
-# write urls here
+from django.urls import path, include
+
+from rest_framework import routers
+
+
+from cinema.views import (
+    CinemaHallViewSet,
+    ActorViewSet,
+    GenreViewSet,
+    MovieViewSet,
+    MovieSessionViewSet,
+    TicketViewSet,
+    OrderViewSet,
+)
+
+app_name = "cinema"
+
+router = routers.DefaultRouter()
+router.register("cinema_halls", CinemaHallViewSet)
+router.register("actors", ActorViewSet)
+router.register("genres", GenreViewSet)
+router.register("movies", MovieViewSet)
+router.register("movie_sessions", MovieSessionViewSet)
+router.register("tickets", TicketViewSet)
+router.register("orders", OrderViewSet)
+
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
